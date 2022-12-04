740ae34a32e652a0d1c5a04d1ccafaa6952eaf2f
Jacob Yeung <jacobyeung@berkeley.edu>
2022-12-04 04:58:59 +0000

    added saving

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 # Import libraries for convolutional neural network and data processing
-# We will use pytorch to create a convolutional neural network training on MNIST dataset. 
+# We will use pytorch to create a convolutional neural network training on MNIST dataset.
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import torch
+import pickle
 
 # Import the MNIST dataset
 from torchvision import datasets, transforms
@@ -13,77 +14,144 @@ import torch.nn as nn
 from torch.autograd import Variable
 from basic_CNN import CNN
 
-device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+
 
 class AddGaussianNoise(object):
-    def __init__(self, mean=0., std=1.):
+    def __init__(self, mean=0.0, std=1.0):
         self.std = std
         self.mean = mean
-        
+
     def __call__(self, tensor):
         return tensor + torch.randn(tensor.size()) * self.std + self.mean
-    
+
     def __repr__(self):
-        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+        return self.__class__.__name__ + "(mean={0}, std={1})".format(
+            self.mean, self.std
+        )
+
+
+class AddMask(object):
+    def __init__(self, mask_size=0.5, random_mask=True):
+        self.mask_size = mask_size
+        self.random_mask = random_mask
+
+    def __call__(self, tensor):
+        mask = torch.ones(tensor.size())
+        if self.random_mask:
+            random_x_start = torch.randint(
+                0, int(tensor.size()[1] * self.mask_size), size=(1,)
+            ).item()
+            random_y_start = torch.randint(
+                0, int(tensor.size()[2] * self.mask_size), size=(1,)
+            ).item()
+            mask[
+                :,
+                random_x_start : random_x_start
+                + int(self.mask_size * tensor.size()[1]),
+                random_y_start : random_y_start
+                + int(self.mask_size * tensor.size()[2]),
+            ] = 0
+        else:
+            mask[
+                :,
+                : int(self.mask_size * tensor.size()[1]),
+                : int(self.mask_size * tensor.size()[2]),
+            ] = 0
+        return tensor * mask
+
+    def __repr__(self):
+        return self.__class__.__name__ + "(mask_size={0})".format(self.mask_size)
+
 
 def get_dataloaders():
-    train_data = datasets.MNIST(root = 'data', train = True, download = True, transform = transforms.ToTensor())
-    test_data = datasets.MNIST(root = 'data', train = False, download = True, transform = transforms.ToTensor())
-    test_data_noisy = datasets.MNIST(root = 'data', train = False, download = True, 
-        transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,)),
-        AddGaussianNoise(0., 1.)
-    ]))
+    train_data = datasets.MNIST(
+        root="data", train=True, download=True, transform=transforms.ToTensor()
+    )
+    test_data = datasets.MNIST(
+        root="data", train=False, download=True, transform=transforms.ToTensor()
+    )
+    test_data_noisy = datasets.MNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,)),
+                AddGaussianNoise(0.0, 1.0),
+            ]
+        ),
+    )
+    test_data_masked = datasets.MNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=transforms.Compose([transforms.ToTensor(), AddMask(0.5, True)]),
+    )
 
     return {
-        'train' : torch.utils.data.DataLoader(train_data, 
-                                            batch_size=512, 
-                                            shuffle=True,),
-        
-        'test'  : torch.utils.data.DataLoader(test_data, 
-                                            batch_size=512, 
-                                            shuffle=True,),
-
-        'test_data_noisy'  : torch.utils.data.DataLoader(test_data_noisy, 
-                                            batch_size=512, 
-                                            shuffle=True,),
+        "train": torch.utils.data.DataLoader(
+            train_data,
+            batch_size=512,
+            shuffle=True,
+        ),
+        "test": torch.utils.data.DataLoader(
+            test_data,
+            batch_size=512,
+            shuffle=True,
+        ),
+        "test_data_noisy": torch.utils.data.DataLoader(
+            test_data_noisy,
+            batch_size=512,
+            shuffle=True,
+        ),
+        "test_data_masked": torch.utils.data.DataLoader(
+            test_data_masked,
+            batch_size=512,
+            shuffle=True,
+        ),
     }
+
 
 def train(num_epochs, cnn, loaders, device):
     loss_func = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(cnn.parameters(), lr = 0.01)
+    optimizer = torch.optim.Adam(cnn.parameters(), lr=0.01)
     cnn.train()
-        
+
     # Train the model
-    total_step = len(loaders['train'])
+    total_step = len(loaders["train"])
     losses = []
-        
+
     for epoch in range(num_epochs):
-        for i, (images, labels) in enumerate(loaders['train']):
-            
+        for i, (images, labels) in enumerate(loaders["train"]):
+
             # gives batch data, normalize x when iterate train_loader
-            b_x = Variable(images).to(device)   # batch x
-            b_y = Variable(labels).to(device)   # batch y
+            b_x = Variable(images).to(device)  # batch x
+            b_y = Variable(labels).to(device)  # batch y
             # b_x = images
             # b_y = labels
-            output = cnn(b_x)[0]               
+            output = cnn(b_x)[0]
             loss = loss_func(output, b_y)
-            
-            # clear gradients for this training step   
-            optimizer.zero_grad()           
-            
-            # backpropagation, compute gradients 
+
+            # clear gradients for this training step
+            optimizer.zero_grad()
+
+            # backpropagation, compute gradients
             loss.backward()
-            # apply gradients             
+            # apply gradients
             optimizer.step()
-            losses.append(loss.item())                
-            
-            if (i+1) % 100 == 0:
-                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                       .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
-            
+            losses.append(loss.item())
+
+            if (i + 1) % 100 == 0:
+                print(
+                    "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
+                        epoch + 1, num_epochs, i + 1, total_step, loss.item()
+                    )
+                )
+
     return losses
+
 
 def test(loaders, label, device):
     # Test the model
@@ -98,15 +166,35 @@ def test(loaders, label, device):
             pred_y = torch.max(test_output, 1)[1].data.squeeze()
             correct += (pred_y == labels).sum().item()
             total += labels.size(0)
-        accuracy = correct / float(total) 
-        print('Test Accuracy of the model on the 10000 test images: %.4f' % accuracy)
+        accuracy = correct / float(total)
+        print("Test Accuracy of the model on the 10000 test images: %.4f" % accuracy)
         return accuracy
+
 
 for num_layers in range(2, 5):
     for num_recurrence in range(3):
         for num_channels in [8, 16, 24]:
-            cnn = CNN(num_layers=num_layers, num_recurrence=num_recurrence, num_channels=num_channels).to(device)
+            cnn = CNN(
+                num_layers=num_layers,
+                num_recurrence=num_recurrence,
+                num_channels=num_channels,
+            ).to(device)
             loaders = get_dataloaders()
-            loss = train(10, cnn, loaders, device)
-            test_acc = test(loaders, 'test', device)
-            noisy_test_acc = test(loaders, 'test_data_noisy', device)
+            loss = train(3, cnn, loaders, device)
+            test_acc = test(loaders, "test", device)
+            noisy_test_acc = test(loaders, "test_data_noisy", device)
+            masked_test_acc = test(loaders, "test_data_masked", device)
+            with open(
+                f"./model_num_layers={num_layers}_num_recurrence={num_recurrence}_num_channels={num_channels}.pkl",
+                "wb",
+            ) as f:
+                pickle.dump(
+                    {
+                        "cnn": cnn,
+                        "loss": loss,
+                        "test_acc": test_acc,
+                        "test_noisy_acc": noisy_test_acc,
+                        "test_masked_acc": masked_test_acc,
+                    },
+                    f,
+                )
